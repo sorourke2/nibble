96fa0e57f544654ed176394a3e1e7e9087655106
sorourke2 <orourke.she@husky.neu.edu>
2020-11-19 18:43:53 -0500

    updated schema, still must update measurements

--- database/schema.sql
+++ database/schema.sql
@@ -16,26 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `dietary_types`
+-- Table structure for table `dietary_type`
 --
 
-DROP TABLE IF EXISTS `dietary_types`;
+DROP TABLE IF EXISTS `dietary_type`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `dietary_types` (
+CREATE TABLE `dietary_type` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(225) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `dietary_types`
+-- Dumping data for table `dietary_type`
 --
 
-LOCK TABLES `dietary_types` WRITE;
-/*!40000 ALTER TABLE `dietary_types` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dietary_types` ENABLE KEYS */;
+LOCK TABLES `dietary_type` WRITE;
+/*!40000 ALTER TABLE `dietary_type` DISABLE KEYS */;
+INSERT INTO `dietary_type` VALUES (1,'keto');
+/*!40000 ALTER TABLE `dietary_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -50,8 +51,8 @@ CREATE TABLE `has_created` (
   `registered_users` int NOT NULL,
   PRIMARY KEY (`recipes`,`registered_users`),
   KEY `has_created_ibfk_2_idx` (`registered_users`),
-  CONSTRAINT `has_created_ibfk_1` FOREIGN KEY (`recipes`) REFERENCES `recipes` (`id`),
-  CONSTRAINT `has_created_ibfk_2` FOREIGN KEY (`registered_users`) REFERENCES `registered_users` (`id`)
+  CONSTRAINT `has_created_ibfk_1` FOREIGN KEY (`recipes`) REFERENCES `recipe` (`id`),
+  CONSTRAINT `has_created_ibfk_2` FOREIGN KEY (`registered_users`) REFERENCES `registered_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -74,10 +75,10 @@ DROP TABLE IF EXISTS `has_dietary_type`;
 CREATE TABLE `has_dietary_type` (
   `recipe` int NOT NULL,
   `dietary_type` int NOT NULL,
-  PRIMARY KEY (`recipe`),
+  PRIMARY KEY (`recipe`,`dietary_type`),
   KEY `has_dietary_type_1_idx` (`dietary_type`),
-  CONSTRAINT `has_dietary_type_1` FOREIGN KEY (`recipe`) REFERENCES `recipes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `has_dietary_type_2` FOREIGN KEY (`dietary_type`) REFERENCES `dietary_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `has_dietary_type_1` FOREIGN KEY (`recipe`) REFERENCES `recipe` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `has_dietary_type_2` FOREIGN KEY (`dietary_type`) REFERENCES `dietary_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -87,6 +88,7 @@ CREATE TABLE `has_dietary_type` (
 
 LOCK TABLES `has_dietary_type` WRITE;
 /*!40000 ALTER TABLE `has_dietary_type` DISABLE KEYS */;
+INSERT INTO `has_dietary_type` VALUES (20,1);
 /*!40000 ALTER TABLE `has_dietary_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -102,8 +104,8 @@ CREATE TABLE `has_saved` (
   `user` int NOT NULL,
   PRIMARY KEY (`recipe`,`user`),
   KEY `specializes_in_ibfk_2_idx` (`user`),
-  CONSTRAINT `has_saved_ibfk_1` FOREIGN KEY (`recipe`) REFERENCES `recipes` (`id`),
-  CONSTRAINT `has_saved_ibfk_2` FOREIGN KEY (`user`) REFERENCES `registered_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `has_saved_ibfk_1` FOREIGN KEY (`recipe`) REFERENCES `recipe` (`id`),
+  CONSTRAINT `has_saved_ibfk_2` FOREIGN KEY (`user`) REFERENCES `registered_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -117,29 +119,28 @@ LOCK TABLES `has_saved` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `ingredients`
+-- Table structure for table `ingredient`
 --
 
-DROP TABLE IF EXISTS `ingredients`;
+DROP TABLE IF EXISTS `ingredient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `ingredients` (
+CREATE TABLE `ingredient` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(225) NOT NULL,
   `measurement` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `ingrdient_1_idx` (`measurement`),
-  CONSTRAINT `ingrdient_1` FOREIGN KEY (`measurement`) REFERENCES `measurements` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `ingredients`
+-- Dumping data for table `ingredient`
 --
 
-LOCK TABLES `ingredients` WRITE;
-/*!40000 ALTER TABLE `ingredients` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ingredients` ENABLE KEYS */;
+LOCK TABLES `ingredient` WRITE;
+/*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES (1,'Ingredient1',NULL),(3,'ingredient_1',NULL),(29,'ingredient_1',NULL),(30,'ingredient_1',NULL),(31,'ingredient_2',NULL),(32,'ingredient_1',NULL),(33,'ingredient_2',NULL),(77,'tomato',1);
+/*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -153,9 +154,9 @@ CREATE TABLE `made_up_of` (
   `ingredient` int NOT NULL,
   `recipe` int NOT NULL,
   PRIMARY KEY (`ingredient`,`recipe`),
-  KEY `made_up_of_1_idx` (`recipe`),
-  CONSTRAINT `made_up_of_ibfk_1` FOREIGN KEY (`recipe`) REFERENCES `recipes` (`id`),
-  CONSTRAINT `made_up_of_ibfk_2` FOREIGN KEY (`recipe`) REFERENCES `ingredients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `recipe` (`recipe`) /*!80000 INVISIBLE */,
+  CONSTRAINT `made_up_of_1` FOREIGN KEY (`recipe`) REFERENCES `recipe` (`id`),
+  CONSTRAINT `made_up_of_2` FOREIGN KEY (`ingredient`) REFERENCES `ingredient` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -165,17 +166,18 @@ CREATE TABLE `made_up_of` (
 
 LOCK TABLES `made_up_of` WRITE;
 /*!40000 ALTER TABLE `made_up_of` DISABLE KEYS */;
+INSERT INTO `made_up_of` VALUES (29,16),(30,19),(31,19),(32,20),(33,20);
 /*!40000 ALTER TABLE `made_up_of` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `measurements`
+-- Table structure for table `measurement`
 --
 
-DROP TABLE IF EXISTS `measurements`;
+DROP TABLE IF EXISTS `measurement`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `measurements` (
+CREATE TABLE `measurement` (
   `id` int NOT NULL,
   `name` varchar(225) NOT NULL,
   `unit` varchar(225) DEFAULT NULL,
@@ -185,22 +187,22 @@ CREATE TABLE `measurements` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `measurements`
+-- Dumping data for table `measurement`
 --
 
-LOCK TABLES `measurements` WRITE;
-/*!40000 ALTER TABLE `measurements` DISABLE KEYS */;
-/*!40000 ALTER TABLE `measurements` ENABLE KEYS */;
+LOCK TABLES `measurement` WRITE;
+/*!40000 ALTER TABLE `measurement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `measurement` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `recipes`
+-- Table structure for table `recipe`
 --
 
-DROP TABLE IF EXISTS `recipes`;
+DROP TABLE IF EXISTS `recipe`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `recipes` (
+CREATE TABLE `recipe` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(225) NOT NULL,
   `difficulty` varchar(225) DEFAULT NULL,
@@ -210,26 +212,27 @@ CREATE TABLE `recipes` (
   `minutes_to_make` varchar(45) DEFAULT NULL,
   `image_source` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `recipes`
+-- Dumping data for table `recipe`
 --
 
-LOCK TABLES `recipes` WRITE;
-/*!40000 ALTER TABLE `recipes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recipes` ENABLE KEYS */;
+LOCK TABLES `recipe` WRITE;
+/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
+INSERT INTO `recipe` VALUES (1,'spaghetti 2.0',NULL,NULL,NULL,NULL,NULL,NULL),(2,'Recipe1',NULL,NULL,NULL,NULL,NULL,NULL),(5,'spaghetti 2.0',NULL,NULL,NULL,NULL,NULL,NULL),(7,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(8,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(10,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(11,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(13,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(16,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(19,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL),(20,'recipe 4',NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `registered_users`
+-- Table structure for table `registered_user`
 --
 
-DROP TABLE IF EXISTS `registered_users`;
+DROP TABLE IF EXISTS `registered_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `registered_users` (
+CREATE TABLE `registered_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(225) DEFAULT NULL,
   `password` varchar(225) DEFAULT NULL,
@@ -238,34 +241,34 @@ CREATE TABLE `registered_users` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `registered_users`
+-- Dumping data for table `registered_user`
 --
 
-LOCK TABLES `registered_users` WRITE;
-/*!40000 ALTER TABLE `registered_users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `registered_users` ENABLE KEYS */;
+LOCK TABLES `registered_user` WRITE;
+/*!40000 ALTER TABLE `registered_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registered_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `unregistered_users`
+-- Table structure for table `unregistered_user`
 --
 
-DROP TABLE IF EXISTS `unregistered_users`;
+DROP TABLE IF EXISTS `unregistered_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `unregistered_users` (
+CREATE TABLE `unregistered_user` (
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `unregistered_users`
+-- Dumping data for table `unregistered_user`
 --
 
-LOCK TABLES `unregistered_users` WRITE;
-/*!40000 ALTER TABLE `unregistered_users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `unregistered_users` ENABLE KEYS */;
+LOCK TABLES `unregistered_user` WRITE;
+/*!40000 ALTER TABLE `unregistered_user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unregistered_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -285,4 +288,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-12 19:20:22
+-- Dump completed on 2020-11-19 18:39:29
