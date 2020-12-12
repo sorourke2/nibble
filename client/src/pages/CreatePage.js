8be39082e9b9ceb888c6ac73ccc776d7a9c5605d
dillonhammer <dillonhammer@gmail.com>
2020-12-12 16:42:12 -0500

    Barebones recipe create page

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -27,16 +27,58 @@ const CreateButton = BasicButton({ hoverColor: "lightblue" });
 
 const CreatePage = () => {
   const [recipeName, setRecipeName] = useState("");
+  const [difficulty, setDifficulty] = useState("Easy");
+  const [cookingMethod, setCookingMethod] = useState("");
+  const [servingSize, setServingSize] = useState(0);
+  const [cuisine, setCuisine] = useState("");
+  const [minutes, setMinutes] = useState(0);
+  const [ingredients, setIngredients] = useState([]);
 
   const onCreate = async () => {
     if (recipeName !== "") {
       const recipe = {
         name: recipeName,
-        ingredients: [],
+        difficulty,
+        cooking_method: cookingMethod,
+        serving_size: servingSize,
+        cuisine,
+        minutes_to_make: minutes,
+        ingredients,
+        dietary_types: [],
       };
       await RecipeService.createRecipe(recipe);
       setRecipeName("");
+      setDifficulty("Easy");
+      setCookingMethod("");
+      setServingSize(0);
+      setCuisine("");
+      setMinutes(0);
+      setIngredients([]);
     }
+  };
+
+  const addIngredient = () => {
+    const tempIngredients = [...ingredients];
+    tempIngredients.push({ name: "", measurement: { unit: "", amount: 0 } });
+    setIngredients(tempIngredients);
+  };
+
+  const changeIngredientField = (value, index, field) => {
+    const tempIngredients = [...ingredients];
+    switch (field) {
+      case "name":
+        tempIngredients[index][field] = value;
+        break;
+      case "unit":
+        tempIngredients[index]["measurement"][field] = value;
+        break;
+      case "amount":
+        tempIngredients[index]["measurement"][field] = value;
+        break;
+      default:
+        break;
+    }
+    setIngredients(tempIngredients);
   };
 
   return (
@@ -50,6 +92,76 @@ const CreatePage = () => {
             placeholder="New Recipe"
             onChange={({ target }) => setRecipeName(target.value)}
           />
+          <div>
+            Difficulty:
+            <select
+              value={difficulty}
+              onChange={({ target }) => setDifficulty(target.value)}
+            >
+              <option value="Easy">Easy</option>
+              <option value="Medium">Medium</option>
+              <option value="Hard">Hard</option>
+            </select>
+          </div>
+          <div>
+            Cooking Method:
+            <input
+              value={cookingMethod}
+              onChange={({ target }) => setCookingMethod(target.value)}
+            />
+          </div>
+          <div>
+            Serving Size:
+            <input
+              type="number"
+              value={servingSize}
+              onChange={({ target }) => setServingSize(target.value)}
+            />
+          </div>
+          <div>
+            Cuisine:
+            <input
+              value={cuisine}
+              onChange={({ target }) => setCuisine(target.value)}
+            />
+          </div>
+          <div>
+            Minutes to Make:
+            <input
+              type="number"
+              value={minutes}
+              onChange={({ target }) => setMinutes(target.value)}
+            />
+          </div>
+          {ingredients.map((ingredient, index) => (
+            <div key={index}>
+              Ingredient Name:{" "}
+              <input
+                value={ingredient.name}
+                onChange={({ target }) =>
+                  changeIngredientField(target.value, index, "name")
+                }
+              />
+              Unit:{" "}
+              <input
+                value={ingredient.measurement.unit}
+                onChange={({ target }) =>
+                  changeIngredientField(target.value, index, "unit")
+                }
+              />
+              Amount:{" "}
+              <input
+                type="number"
+                value={ingredient.measurement.amount}
+                onChange={({ target }) =>
+                  changeIngredientField(target.value, index, "amount")
+                }
+              />
+            </div>
+          ))}
+          <div>
+            <button onClick={addIngredient}>Add Ingredient</button>
+          </div>
         </div>
         <br />
         <CreateButton onClick={onCreate}>Create</CreateButton>
